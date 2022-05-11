f97e37d0166f251ba3679cc7b7ea50910a83f999
Daniel Stock <daniel.stock@tooloop.de>
2022-05-11 11:04:22 +0200

    install process error handling and prettyfication

--- static/js/appcenter.js
+++ static/js/appcenter.js
@@ -20,11 +20,12 @@ const ControlPanel = Vue.createApp({
                 isActive: false,
                 progress: 0,
                 isFinished: false,
+                success: true,
                 title: "",
                 package: null,
                 log: "",
                 lastMessage: "",
-                progressEventSource: null
+                progressEventSource: null,
             }
         }
     },
@@ -58,42 +59,60 @@ const ControlPanel = Vue.createApp({
         this.availablePackages = availablePackages;
     },
 
+    mounted() {
+        console.log(this.$refs.log);
+    },
+
     methods: {
 
         install(package) {
-            // Show modal
-            this.showModal("Installing", package);
+            this.performInstall("install", package);
 
-            // Trigger installation
-            fetch(this.api + "/appcenter/install/" + package.packageName)
-                .then(response => response.json())
-                .then(data => { this.finishInstallation(data.message) });
+        },
+
+        uninstall(package) {
+            this.performInstall("uninstall", package);
+        },
+
+        performInstall(method, package) {
+            // Reset installer states
+            this.installer.package = package;
+            this.installer.isFinished = false;
+            this.installer.success = true;
+            this.installer.progress = 0;
+            this.installer.title = this.capitalize(method) + "ing " + package.name;
+            this.installer.log = "";
+            this.installer.lastMessage = "";
+
+            // Show modal
+            this.installer.isActive = true;
+
+            // Perform installation
+            fetch(this.api + "/appcenter/" + method + "/" + package.packageName)
+                .then(response => {
+                    if (response.status != 200) this.installer.success = false;
+                    return response.json()
+                })
+                .then(data => { this.finishInstallation(data.message) })
+                .catch((error) => {
+                    this.installer.success = false;
+                    this.finishInstallation(error);
+                });
 
             // Listen to progress updates
             this.installer.progressEventSource = new EventSource("/tooloop/api/v1.0/appcenter/progress");
             this.installer.progressEventSource.onmessage = this.updateProgress;
         },
 
-        uninstall(package) {
-            // Show modal
-            this.showModal("Uninstalling", package);
-
-            // Trigger uninstallation
-            fetch(this.api + "/appcenter/uninstall/" + package.packageName)
-                .then(response => response.json())
-                .then(data => { this.finishInstallation(data.message) });
-
-            // Listen to progress updates
-            this.installer.progressEventSource = new EventSource("/tooloop/api/v1.0/appcenter/progress");
-            this.installer.progressEventSource.onmessage = this.updateProgress;
+        capitalize(s) {
+            if (typeof s !== 'string') return ''
+            return s.charAt(0).toUpperCase() + s.slice(1)
         },
 
         updatePackages() {
             fetch(this.api + '/reload')
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data.message)
-
                     fetch(this.api + "/appcenter/installed")
                         .then(response => response.json())
                         .then(data => this.installedPresentation = data);
@@ -104,33 +123,39 @@ const ControlPanel = Vue.createApp({
                 });
         },
 
-        showModal(title, package) {
-            this.installer.package = package;
-            this.installer.isFinished = false;
-            this.installer.progress = 0;
-            this.installer.title = title + " " + package.name;
-            this.installer.log = "";
-            this.installer.lastMessage = "";
-            this.installer.isActive = true;
-        },
-
         updateProgress(event) {
             progress = JSON.parse(event.data);
             this.installer.progress = progress.percent;
+            this.log(progress.task);
 
-            if (this.installer.lastMessage != progress.task) {
-                this.installer.lastMessage = progress.task;
-                this.installer.log += "\n" + progress.task;
-            }
         },
 
         finishInstallation(message) {
             this.installer.progressEventSource.close();
-            this.installer.log += "\n\n" + message;
             this.installer.isFinished = true;
+            this.installer.progress = 100;
+
+            let icon = this.installer.success ?
+                '<span class="icon has-text-success"><i class="fas fa-check"></i></span>' :
+                '<span class="icon has-text-danger"><i class="fas fa-xmark"></i></span>';
+
+            this.log(`<p class="icon-text has-text-light mt-5">${icon}${message}</p>`);
 
             this.updatePackages();
         },
+
+        log(message) {
+            if (this.installer.lastMessage != message) {
+                this.installer.lastMessage = message;
+                if (this.installer.log != "") this.installer.log += "<br>";
+                this.installer.log += message;
+                // scrolling needed to wait a little for some reason
+                // maybe the DOM had to catch up to update scrollTopMax?!
+                setTimeout(() => {
+                    this.$refs.log.scrollTop = this.$refs.log.scrollTopMax;
+                }, 16);
+            }
+        }
 
     }
 
