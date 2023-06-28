4fea52e3edebb6f43186aa2da0b9d4a7bd36c9e7
vollstock <daniel.stock@tooloop.de>
2023-06-28 16:18:40 +0200

    debug mode for Flask 2.3+

--- app.py
+++ app.py
@@ -526,5 +526,6 @@ if __name__ == "__main__":
     app.run(
         host=app.config['HOST'],
         port=app.config['PORT'],
+        debug=app.config['DEBUG'],
         extra_files=[os.path.join(app.root_path, 'data', 'reload')]
     )
